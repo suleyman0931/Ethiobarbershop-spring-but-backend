01fe3765ea5a126b879f68cdfe752572c399b9e3
Suleyman Abdu <suleymanabdu0931@gmail.com>
2026-05-09 10:37:47 +0300

    Prepare for deployment: WAR packaging, SSL config, time format updates

--- backend/src/main/java/com/barbershop/BarbershopBackendApplication.java
+++ backend/src/main/java/com/barbershop/BarbershopBackendApplication.java
@@ -2,13 +2,20 @@ package com.barbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaAuditing // ← THIS ALLOWS TIMESTAMPS
-public class BarbershopBackendApplication {
+public class BarbershopBackendApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(BarbershopBackendApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(BarbershopBackendApplication.class, args);
