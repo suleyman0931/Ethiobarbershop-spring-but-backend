4d54e84c2576ac668beed51be5db6c762ee9ceb0
Suleyman Abdu <suleymanabdu0931@gmail.com>
2026-05-09 10:54:36 +0300

    Configure for Railway deployment: JAR packaging, environment variables

--- backend/src/main/java/com/barbershop/BarbershopBackendApplication.java
+++ backend/src/main/java/com/barbershop/BarbershopBackendApplication.java
@@ -2,20 +2,13 @@ package com.barbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@EnableJpaAuditing // ← THIS ALLOWS TIMESTAMPS
-public class BarbershopBackendApplication extends SpringBootServletInitializer {
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(BarbershopBackendApplication.class);
-  }
+@EnableJpaAuditing
+public class BarbershopBackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BarbershopBackendApplication.class, args);
